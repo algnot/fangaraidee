a437ce259eb4badfd67cc9f9dbcd0c65fc727e72
algnot <thanawat.k2000@gmail.com>
2021-10-18 16:22:23 +0700

    new commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navbar from "./component/Navbar/Navbar";
 import styles from "./App.module.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./component/Home/Home";
-import Test from "./component/Test/Test";
+import Test from './component/Test/Home'
 
 function App() {
   const [loading, setLoading] = useState(false);
@@ -21,17 +21,14 @@ function App() {
         <div className={styles.navbar}>
           <Navbar />
         </div>
-
         <Switch>
           <Route exact path="/">
-            <Home setLoading={setLoading} />
+            <Home setLoading={setLoading} id="mTdG3yuGbp2GBUNMggMg" />
           </Route>
-
           <Route path="/test">
-            <Test/>
+            <Test setLoading={setLoading} id="IR1X6Fvydxwmhvsfhegz"/>
           </Route>
         </Switch>
-        
       </div>
     </Router>
   );
--- src/component/Test/Home.js
+++ src/component/Test/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import styles from "./Home.module.css";
 import SearchBar from './../SearchBar/SearchBar'
 import ChatBar from "../ChatBar/ChatBar"
+import Player from "../Player/Player";
 
 export default function Home({setLoading , id}) {
   return (
     <div className={styles.container}>
       <div className={styles.mainMenu}>
         <SearchBar/>
+        <Player id={id}/>
       </div>
 
       <div className={styles.globalChat}>
